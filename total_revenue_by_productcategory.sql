8d0064487cb09a6c5e57b2fccddbf1978ba683a3
Yetunde Oluyemi <oluyemichristianah@gmail.com>
2023-06-27 13:56:55 +0100

    removed deleted files

--- total_revenue_by_productcategory.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO "untitled_name" ("total_revenue", "product_category") VALUES
-('419719922.282712', 'timepieces');
-INSERT INTO "untitled_name" ("total_revenue", "product_category") VALUES
-('229493549.167652', 'necklaces');
-INSERT INTO "untitled_name" ("total_revenue", "product_category") VALUES
-('112581424.452179', 'bracelets');
